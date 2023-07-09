c61199ee25c33fe934b6cc91f8804b94e78f6aae
Alejandroxc <ya.canizalez@duocuc.cl>
2023-07-08 22:09:48 -0400

    se han subido todos los html y añadido contenido en ellos

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,22 @@ urlpatterns = [
     path('', index, name="index"),
     path('registro/', registro, name='registro'),
     path('ingresar/', ingresar, name='ingresar'),
-    path('menu-superior/' menuSuperior, name= 'menu-superior'),
+    path('menu-superior/', menuSuperior, name='menu-superior'),
+    path('administracion/', administracion, name='administracion'),
+    path('bodega/', bodega, name= 'bodega'),
+    path('boleta/', boleta, name='boleta'),
+    path('boletaCli/', boletaCli, name='boletaCli'),
+    path('carrito/', carrito, name='carrito'),
+    path('cerrarSesion/', cerrarSesion, name='cerrarSesion'),
+    path('ficha/', ficha, name='ficha'),
+    path('footer/', footer, name='footer'),
+    path('misCompras/', misCompras, name='misCompras'),
+    path('misDatos/', misDatos, name='misDatos'),
+    path('nosotros/', nosotros, name='nosotros'),
+    path('productos/', producto, name='productos'),
+    path('ropa/', ropa, name='ropa'),
+    path('usuarios/', ropa, name='usuarios'),
+    path('ventas/', ventas, name='ventas'),
+
+
 ]
--- app/views.py
+++ app/views.py
@@ -13,3 +13,48 @@ def ingresar(request):
 
 def menuSuperior(request):
     return render(request, 'app/menu-superior.html')
+
+def administracion(request):
+    return render(request, 'app/administracion.html')
+
+def bodega(request):
+    return render(request, 'app/bodega.html')
+
+def boleta(request):
+    return render(request, 'app/boleta.cli')
+
+def boletaCli(request):
+    return render(request, 'app/boletaCli.html')
+
+def carrito(request):
+    return render(request, 'app/carrito.html')
+
+def cerrarSesion(request):
+    return render(request, 'app/cerrarSesion.html')
+
+def ficha(request):
+    return render(request, 'app/ficha.html')
+
+def misCompras(request):
+    return render(request, 'app/misCompras.html')
+
+def misDatos(request):
+    return render(request, 'app/misDatos.html')
+
+def producto(request):
+    return render(request, 'app/producto.html')
+
+def ropa(request):
+    return render(request, 'app/ropa.html')
+
+def usuario(request):
+    return render(request, 'app/usuario.html')
+
+def ventas(request):
+    return render(request, 'app/ventas.html')
+
+def footer(request):
+    return render(request, 'app/footer.html')
+
+def nosotros(request):
+    return render(request, 'app/nosotros.html')
