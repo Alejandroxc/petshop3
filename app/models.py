f1fbe720ebc771584e591973860d1f5c6068f192
fabiancitoo <fab.salazara@duocuc.cl>
2023-07-10 20:30:02 -0400

    cambios en models

--- app/models.py
+++ app/models.py
@@ -18,3 +18,18 @@ class Prodcuto(models.Model):
 
     def __str__(self):
         return self.nombre
+    
+class Usuario(models.Model):
+    id = models.AutoField(primary_key=True)
+    es_administrador = models.BooleanField(default=False) # Aquí es donde se realizó el cambio
+    rut = models.CharField(max_length=12)
+    nombres = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    correo = models.EmailField(max_length=100)
+    direccion = models.CharField(max_length=100)
+    contrasena = models.CharField(max_length=50)
+    imagen = models.ImageField(upload_to="usuarios", null=True)
+    suscripcion = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.nombres} {self.apellidos}"
